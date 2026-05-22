cc3e66af1857dafac56c5f406f494f07fc4e9c40
yaroslavdubeniuk-ctrl <yaroslav.dubeniuk@gmail.com>
2026-05-22 04:27:30 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Commented out so React doesn't look for an empty #root div on your new single-page form
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+*/
